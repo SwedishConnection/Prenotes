f1ed118b03ae01e47c29c11d58b666b9cdd88a56
MatthewDavidYoung <matthewdyoung@gmail.com>
2014-03-21 21:21:35 +0100

    Basic setup

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+/**
+ * Prenotes: Pre-Notes is shorthand for Pre-school Notifications.  
+ * The project hopes to support both staff and parents communicate
+ * online.  The majority of communication is notifications. 
+ */
+var express = require('express');
+
+
+var app = express();
+app.configure(function() {
+	app.use(express.static(__dirname + '/views'));
+});
+
+
+console.log("Listening on port %d", process.env.PORT || 4730);
+app.listen(process.env.PORT || 4730);
