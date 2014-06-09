c2149fc82291fc2cdfbbe03f8013b661cc102cc3
MatthewDavidYoung <matthewdyoung@gmail.com>
2014-06-09 13:43:36 +0200

    Added Apache 2.0 license

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 /**
- * Prenotes: Pre-Notes is shorthand for Pre-school Notifications.
- * The project hopes to support both staff and parents communicate
- * online.  The majority of communication is notifications.
- */
+   Copyright 2014 Swedish Connection
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 
 /**
  * Third-party modules
